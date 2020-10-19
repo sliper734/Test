853b2120ad263f03987f3024b81ccd5421ebf65e
Sergey <sliper734@gmail.com>
2020-10-19 18:43:11 +0300

    19.10
    Перенесенный проект на Blazor.  Без миграции.

---
+++ Models/MySqlDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Models
+{
+    public class MySqlDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        public MySqlDbContext()
+        {
+            Database.EnsureDeleted();   
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;user id=root;password=sliper98;persistsecurityinfo=True;database=new_schema;");
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Email { get; set; }
+    }
+}
